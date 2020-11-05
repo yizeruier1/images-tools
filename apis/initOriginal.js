458357d7bcfe5dceb9cdbd95511d4455894600fc
yizeruier1 <gostephen@163.com>
2020-11-05 17:54:02 +0800

    支持超长名字的原创声明文件生成

--- apis/initOriginal.js
+++ apis/initOriginal.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 function generateOriginal (req, res) {
     const {name, path} = req.query
     const names = name.split('--')
+    const longNames = []
     const time = new Date()
     const year = time.getFullYear()
     const month = time.getMonth() + 1
@@ -14,25 +15,33 @@ function generateOriginal (req, res) {
     const letterWidthEn = 9  // 单个英文字母宽度
 
     registerFont('./assets/simhei.ttf', { family: 'simhei' })
-    loadImage('./assets/originalStatement.jpg').then(img => {
+    if (names[0] && names[0].length > 9) {
+        longNames.push(names[0])
+        names[0] = '__'
+    }
+    if (names[1] && names[1].length > 9) {
+        longNames.push(names[1])
+        names[1] = '__'
+    }
+
+    longNames.length > 0 && loadImage('./assets/originalStatement_.jpg').then(img => {
         const imgw = img.width
         const imgh = img.height
-        names.forEach((item, index) => {
+        const timeStr = `${year} 年 ${month} 月 ${date} 日`
+        longNames.forEach((item, index) => {
             const canvas = createCanvas(imgw, imgh)
             const ctx = canvas.getContext('2d')
             ctx.drawImage(img, 0, 0, imgw, imgh)
-            ctx.fillStyle = "#838381"
-            ctx.font = '18px simhei'
-            const x = index === 0 ? (166 - item.length * letterWidth) / 2 : (166 - item.length * letterWidthEn) / 2
+            ctx.fillStyle = "#727a7d"
+            ctx.font = '23px simhei'
+            const x = (/^[a-zA-Z]/).test(item) ? (396 - item.length * letterWidthEn) / 2 : (396 - item.length * letterWidth) / 2
             // 名字
-            ctx.fillText(item, nameX + x, 197)
-            //时间
-            ctx.font = '20px simhei'
-            ctx.fillText(year, 538, 882)
-            ctx.fillText(month, 614, 882)
-            ctx.fillText(date, 668, 882)
+            ctx.fillText(item, 380 + x, 274)
+            // 时间
+            ctx.font = '23px simhei'
+            ctx.fillText(timeStr, 774, 1186)
 
-            const outputName = index === 0 ? '/原创声明.jpg' : '/原创声明_EN.jpg'
+            const outputName = `/原创声明_${item}.jpg`
             fs.writeFile(path + outputName, canvas.toBuffer(), (err) => {
                 if (err) {
                     console.log(`${outputName} 处理失败，原因：${err}`)
@@ -40,6 +49,37 @@ function generateOriginal (req, res) {
                     console.log(`${outputName} 处理成功！`)
                 }
             })
+        })
+    })
+
+    names.length > 0 && loadImage('./assets/originalStatement.jpg').then(img => {
+        const imgw = img.width
+        const imgh = img.height
+        names.forEach((item, index) => {
+            if (item !== '__') {
+                const canvas = createCanvas(imgw, imgh)
+                const ctx = canvas.getContext('2d')
+                ctx.drawImage(img, 0, 0, imgw, imgh)
+                ctx.fillStyle = "#838381"
+                ctx.font = '18px simhei'
+                const x = index === 0 ? (166 - item.length * letterWidth) / 2 : (166 - item.length * letterWidthEn) / 2
+                // 名字
+                ctx.fillText(item, nameX + x, 197)
+                //时间
+                ctx.font = '20px simhei'
+                ctx.fillText(year, 538, 882)
+                ctx.fillText(month, 614, 882)
+                ctx.fillText(date, 668, 882)
+
+                const outputName = index === 0 ? `/原创声明_${item}.jpg` : `/原创声明_${names[0]}_EN.jpg`
+                fs.writeFile(path + outputName, canvas.toBuffer(), (err) => {
+                    if (err) {
+                        console.log(`${outputName} 处理失败，原因：${err}`)
+                    } else {
+                        console.log(`${outputName} 处理成功！`)
+                    }
+                })
+            }
         })
     })
 }
